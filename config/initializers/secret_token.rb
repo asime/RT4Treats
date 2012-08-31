503c7c50791c06f7892f32e34edba83179883aca
Arin Sime <asime@o19s.com>
2012-08-31 13:50:32 -0400

    first commit of rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RT4Treats::Application.config.secret_token = '2e4706865438e3ba0527b6e2fce913fd91b967bbd66ac209a418b470f1eb4640d6b519965cfee00f42f1a846f40a680e913e74166aafbd59c7847319c3dc4b6e'
